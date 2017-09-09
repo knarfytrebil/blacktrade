71a356147e21b5496dcf51c036d3ff438c7c5635
Frank Yao <frank@neon.local>
2017-09-10 03:26:28 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 // A WebSocket client that sends one message then closes
 extern crate ws;
+extern crate env_logger;
 
 use ws::{connect, CloseCode};
 
 fn main() {
-    connect("wss://api2.poloniex.com", |out| {
+    // Setup logging
+    env_logger::init().unwrap();
+
+    if let Err(error) = connect("wss://api2.poloniex.com", |out| {
         move |msg| {
             println!("Got message: {}", msg);
             out.close(CloseCode::Normal)
         }
-    }).unwrap()
+    }) {
+        // Inform the user of failure
+        println!("Failed to create WebSocket due to: {:?}", error);
+    }
 } 
