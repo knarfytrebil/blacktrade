43ffaf58c3ed158017f81dccff20d8987e4135c8
Frank Yao <frank@neon.local>
2017-09-10 02:59:05 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Websocket Client
+extern crate ws;
+use ws::listen;
+
 fn main() {
-    println!("Hello, world!");
-}
+    listen("127.0.0.1:3012", |out| {
+        move |msg| {
+            out.send(msg)
+        }
+    }).unwrap()
+} 
