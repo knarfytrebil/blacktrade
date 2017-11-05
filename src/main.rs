444b64b9bce13d40f833ca83a915f3f476833001
Frank <knarfytrebil@gmail.com>
2017-11-05 16:32:59 +0800

    split function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate ws;
 extern crate json;
 
-// use ws::{connect, Handler, Sender, Handshake, Result, Message, CloseCode};
 use ws::{connect, Handler, Sender, Handshake, Result, Message};
 
 // Here we explicity indicate that the Client needs a Sender,
@@ -10,25 +9,35 @@ struct Client {
     out: Sender,
 }
 
-fn parse_data(raw: Message) {
+fn parse_market_data(mkt_data: json::JsonValue) {
+    println!("Got MarketData: {}", mkt_data)
+}
+
+fn parse_raw(raw: Message) {
     // msg -> String -> &str -> enum
     let msg = &String::from(raw.as_text().unwrap());
-    let parsed = json::parse(&*msg).unwrap();
-    println!("Got message: {}", parsed);
+    let parsed_raw = json::parse(&*msg).unwrap();
+
+    {
+        let channel = &parsed_raw[0];
+        if channel == 1010 {
+            println!("Got Heartbeat");
+            return;
+        }
+    }
+
+    parse_market_data(parsed_raw)
+
 }
 
 impl Handler for Client {
-
     fn on_open(&mut self, _: Handshake) -> Result<()> {
         self.out.send(r#"{"command":"subscribe","channel":"BTC_ETH"}"#)
     }
-
     fn on_message(&mut self, msg: Message) -> Result<()> {
-        parse_data(msg);
+        parse_raw(msg);
         Ok(())
-        // self.out.close(CloseCode::Normal)
     }
-
 }
 
 fn main() {
