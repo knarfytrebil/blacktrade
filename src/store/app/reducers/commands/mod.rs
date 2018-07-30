726b8240a0b34e683adb624e21216904db253666
Weiliang Li <to.be.impressive@gmail.com>
2018-07-30 17:52:32 +0900

    [Fix] non existing command

--- src/store/app/reducers/commands/mod.rs
+++ src/store/app/reducers/commands/mod.rs
@@ -3,7 +3,14 @@ use store::app::AppState;
 impl AppState {
     pub fn command_handler(&mut self, cmd: String) -> Option<()> {
         debug!("got command {:?}", &cmd);
-        self.cmd_reg[&cmd](self, "test".to_string());
-        None
+        match self.cmd_reg.contains_key(&cmd) {
+            true => {
+                self.cmd_reg[&cmd](self, "test".to_string());
+                return Some(());
+            }
+            false => {
+                return None;
+            }
+        }
     }
 }
