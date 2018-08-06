b7d1a122c545c980f7a92ac64f95da6a444e3995
Relaed <knarfytrebil@gmail.com>
2018-08-06 14:09:22 +0900

    (fix) closure example

--- examples/closure.rs
+++ examples/closure.rs
@@ -3,21 +3,23 @@ type Reducer =  Fn(i32, i32) -> i32;
 // fn multiplier(state: i32, action: i32) -> i32 { state * action }
 // fn adder(state: i32, action: i32) -> i32 { state + action }
 
-// fn combined_reducer(reducers: &Vec<Reducer>) -> Box<Fn(i32, i32) -> i32> {
-fn combined_reducer(reducers: Vec<Reducer>) -> Reducer {
-    |mut state, action| {
-        for reducer in reducers {
+fn combined_reducer(reducers: Vec<Box<Reducer>>) -> Box<Reducer> {
+    Box::new(move |mut state, action| {
+        for reducer in &reducers {
             state = reducer(state, action)    
         }
         state
-    }
+    })
 }
 
 fn main() {
     let multiply_reducer = |state: i32, action: i32| -> i32 { state * action };
-    let adder = |state: i32, action: i32| -> i32 { state + action };
+    let plus_reducer = |state: i32, action: i32| -> i32 { state + action };
 
-    let reducers: Vec<Reducer> = vec![adder];
+    let reducers: Vec<Box<Reducer>> = vec![
+        Box::new(plus_reducer),
+        Box::new(multiply_reducer)
+    ];
     let num = combined_reducer(reducers)(1, 3);
     println!("{}", num);
 }
