d87ad14a0164fbae45a83defa4f0f303a00e52c0
Frank <knarfytrebil@gmail.com>
2018-08-23 21:37:48 +0900

    (fix) capture panic in child process

--- src/middlewares/debug.rs
+++ src/middlewares/debug.rs
@@ -11,13 +11,13 @@ impl Middleware<AppState> for DebugMiddleWare {
         action: AppAction,
         next: &DispatchFunc<AppState>,
     ) -> Result<AppState, String> {
-        debug!("1 {:?}", &action);
+        // debug!("1 {:?}", &action);
         let state = store.get_state();
         debug!("[ACT]: {:?}", &action);
-        debug!("[cmd_str_queue]: {:?} Items", &state.cmd_str_queue.len());
-        debug!("[cmd_running]: {:?} Items", &state.cmd_running.len());
-        debug!("[cmd_ended]: {:?} Items", &state.cmd_ended.len());
-        debug!("[cmd_bar]: {:?}", &state.command);
+        // debug!("[cmd_str_queue]: {:?} Items", &state.cmd_str_queue.len());
+        // debug!("[cmd_running]: {:?} Items", &state.cmd_running.len());
+        // debug!("[cmd_ended]: {:?} Items", &state.cmd_ended.len());
+        // debug!("[cmd_bar]: {:?}", &state.command);
         return next(store, action);
     }
 }
