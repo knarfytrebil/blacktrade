069d54247582674410b668947635322c6f64f90b
Frank <knarfytrebil@gmail.com>
2018-08-27 10:41:27 +0900

    add scroll

--- src/components/command_output.rs
+++ src/components/command_output.rs
@@ -9,12 +9,12 @@ pub mod instance {
     use tui::Terminal;
 
     pub fn render(t: &mut Terminal<MouseBackend>, app: &AppState, area: &Rect) {
-        let scroll = match (app.console_txt.lines().count() as u16).checked_sub( area.height.clone()) {
+        let scroll = match (app.console_txt.lines().count() as u16).checked_sub(area.height.clone()) {
             None => { 0 as u16 } 
             Some(x) if x > 0 as u16 => { x } 
             Some(_) => { 0 }
         };
-        let _paragraph = Paragraph::default()
+        Paragraph::default()
             .block(Block::default())
             .wrap(true)
             .text(&app.console_txt)
