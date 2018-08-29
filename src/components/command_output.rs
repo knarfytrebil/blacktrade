a9d85b98a5744c0c622df2c6841579bc721371ae
Relaed <knarfytrebil@gmail.com>
2018-08-29 16:40:59 +0900

    fix buffer

--- src/components/command_output.rs
+++ src/components/command_output.rs
@@ -16,22 +16,16 @@ pub mod instance {
     }
 
     fn get_buffer(area_height: u16, txt: String) -> String {
-        debug!("txt_len, incoming:{:?}", txt.len());
         let mut lines: Vec<usize> = txt.lines().map(|line| { line.len() }).collect();
-        debug!("lines:{:?}", lines);
         let line_count = lines.len();
-        debug!("line_count:{:?}", line_count);
         let drained: Vec<usize> = match (line_count as u16).checked_sub(area_height) {
+            Some(x) if x >= 0 as u16 => { lines.drain(x as usize..line_count).collect() }
             None | Some(_) => { lines }
-            Some(x) if x >= 0 as u16 => { lines.drain(line_count - (area_height as usize)..line_count).collect() }
         };
-        debug!("drained:{:?}", drained);
-        let drained_bytes: usize = drained.iter().fold(0, |sum, i| sum + i + 2);
-        debug!("drained_bytes:{:?}", drained_bytes);
-        debug!("txt_len:{:?}", txt.len());
+        let drained_bytes: usize = drained.iter().fold(0, |sum, i| sum + i);
         match txt.len() {
             l if l <= drained_bytes => { txt }
-            l if l > drained_bytes => { txt[l - drained_bytes..l].to_string() }
+            l if l > drained_bytes => { txt[l - drained_bytes - drained.len()..l].to_string() }
             _ => { txt }
         }
     }
