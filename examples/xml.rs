e7d022321bbba1f1c87d2a2d75237abce8616384
Relaed <knarfytrebil@gmail.com>
2019-05-27 17:56:16 +0900

    (experiment) add experimental components

--- examples/xml.rs
+++ examples/xml.rs
@@ -1,11 +1,11 @@
 extern crate minidom; 
 
 use std::fs;
-use minidom:Element;
-
-let dom_data = fs::read_to_string("./examples/components/articles.html")
-    .expect("Error reading file");
+use minidom::Element;
 
 fn main() {
+    let dom_data = fs::read_to_string("./examples/components/articles.html")
+        .expect("Error reading file");
     let root: Element = dom_data.parse().unwrap();
+    println!("{:#?}", root);
 }
