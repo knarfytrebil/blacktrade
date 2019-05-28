b4ac7d276cf1dac86f92cba12eca02ecfec9d17a
Relaed <knarfytrebil@gmail.com>
2019-05-28 10:50:46 +0900

    (fix)rename xml file

--- examples/xml.rs
+++ examples/xml.rs
@@ -4,7 +4,7 @@ use std::fs;
 use minidom::Element;
 
 fn main() {
-    let dom_data = fs::read_to_string("./examples/components/articles.html")
+    let dom_data = fs::read_to_string("./examples/components/articles.xml")
         .expect("Error reading file");
     let root: Element = dom_data.parse().unwrap();
     println!("{:#?}", root);
