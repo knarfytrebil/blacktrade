79718228fba4c581407c3d73f0d93052908b0ebb
Relaed <knarfytrebil@gmail.com>
2019-07-09 13:25:04 +0900

    (wip) cleanup classes

--- examples/xml.rs
+++ examples/xml.rs
@@ -203,9 +203,9 @@ fn create_basic_element(el: &Element, parser: TuiParser) {
 
 // Create Custom Element
 fn create_custom_element(element: &Element, parser: TuiParser) {
-    println!("<====== Custom");
+    println!("======> Custom");
     println!("Custom Element ({:#?})", element.name());
-    println!("Custom ======>");
+    println!("Custom <======");
 }
 
 // Utility Functions
