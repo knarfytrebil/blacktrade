35c0ef94636574a27c59e117406c54cc74896eb6
Yao Yuan <yao.y@simplex.ne.jp>
2020-06-23 16:21:15 +0900

    fix format

--- src/reducers/command_bar.rs
+++ src/reducers/command_bar.rs
@@ -1,39 +1,47 @@
-use structs::app::{AppState};
-use reducers::ReducerFn;
 use actions::AppAction;
+use reducers::ReducerFn;
+use structs::app::AppState;
 
 pub fn set() -> Box<ReducerFn> {
-    Box::new(|mut state: AppState, action: &AppAction| -> Result<AppState, String> {
-        match action {
-            AppAction::CommandBarSet(str_ref) => {
-                state.command = str_ref.to_string();
-                Ok(state)
+    Box::new(
+        |mut state: AppState, action: &AppAction| -> Result<AppState, String> {
+            match action {
+                AppAction::CommandBarSet(str_ref) => {
+                    state.command = str_ref.to_string();
+                    Ok(state)
+                }
+                _ => Ok(state),
             }
-            _ => { Ok(state) }
-        }
-    })
+        },
+    )
 }
 
 pub fn push() -> Box<ReducerFn> {
-    Box::new(|mut state: AppState, action: &AppAction| -> Result<AppState, String> {
-        match action {
-            AppAction::CommandBarPush(_char) => {
-                state.command.push(*_char);
-                Ok(state)
+    Box::new(
+        |mut state: AppState, action: &AppAction| -> Result<AppState, String> {
+            match action {
+                AppAction::CommandBarPush(_char) => {
+                    state.command.push(*_char);
+                    Ok(state)
+                }
+                _ => Ok(state),
             }
-            _ => { Ok(state) }
-        }
-    })
+        },
+    )
 }
 
 pub fn enqueue_cmd() -> Box<ReducerFn> {
-    Box::new(|mut state: AppState, action: &AppAction| -> Result<AppState, String> {
-        match action {
-            AppAction::CommandBarEnqueueCmd(uuid) => {
-                state.cmd_str_queue.insert(uuid.clone(), state.command.split_off(1));
-                Ok(state)
+    Box::new(
+        |mut state: AppState, action: &AppAction| -> Result<AppState, String> {
+            match action {
+                AppAction::CommandBarEnqueueCmd(uuid) => {
+                    state
+                        .cmd_str_queue
+                        .insert(uuid.clone(), state.command.split_off(1));
+                    Ok(state)
+                }
+                _ => Ok(state),
             }
-            _ => { Ok(state) }
-        }
-    })
+        },
+    )
 }
