3276749b6d3ee878849fe5b4f698c91c0597c700
Thomas Anderson <knarfytrebil@gmail.com>
2020-07-06 00:00:48 +0900

    modify some stuff

--- src/components/app.rs
+++ src/components/app.rs
@@ -5,7 +5,7 @@ use structs::app::AppState;
 use tui::backend::Backend;
 use tui::layout::{Constraint, Direction, Layout};
 use tui::style::{Color, Style};
-use tui::widgets::Tabs;
+use tui::widgets::{Tabs, Block, Borders};
 use tui::Frame;
 
 pub fn render<B>(frame: &mut Frame<B>, app: &AppState)
@@ -16,7 +16,7 @@ where
         .direction(Direction::Vertical)
         .constraints(
             [
-                Constraint::Length(1),
+                Constraint::Length(2),
                 Constraint::Min(1),
                 Constraint::Length(1),
                 Constraint::Length(1),
@@ -27,6 +27,7 @@ where
 
     let tabs = Tabs::default()
         .titles(&app.tabs.titles)
+        .block(Block::default().borders(Borders::BOTTOM))
         .style(Style::default().fg(Color::Green))
         .highlight_style(Style::default().fg(Color::Yellow))
         .select(app.tabs.selection);
