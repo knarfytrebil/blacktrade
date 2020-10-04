3ecbf5715436b70569b0cc354b57ac097a70e6df
Thomas Anderson <knarfytrebil@gmail.com>
2020-10-04 16:48:49 +0900

    fix

--- src/middlewares/console.rs
+++ src/middlewares/console.rs
@@ -23,21 +23,12 @@ impl Middleware<AppState> for ConsoleMiddleWare {
                     }
                 }
             }
-            &AppAction::CommandCreate(ref uuid) => {
+            &AppAction::CommandCreate(ref uuid) | &AppAction::CommandInvalid(ref uuid) => {
                 let cmd_str = store.get_state().cmd_str_queue[uuid].clone();
-                let prompt_in = format_output!("green", uuid, &cmd_str);
+                let prompt_in = format_output!("white", uuid, &cmd_str);
                 let _ = store.dispatch(AppAction::ConsolePush(prompt_in));
             }
-            &AppAction::CommandInvalid(ref uuid) => {
-                let cmd_str = store.get_state().cmd_str_queue[uuid].clone();
-                let prompt_in = format_output!("red", uuid, &cmd_str);
-                let _ = store.dispatch(AppAction::ConsolePush(prompt_in));
-            }
-            &AppAction::CommandEnd {
-                ref uuid,
-                ref success,
-                ref reason,
-            } => {
+            &AppAction::CommandEnd { ref uuid, ref success, ref reason, } => {
                 let prompt_in = match success {
                     true => format_output!("green", uuid, "Process Terminated"),
                     false => format_output!("red", uuid, reason),
