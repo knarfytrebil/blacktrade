2bfd940dde98fe95964510b3b05ec96deff1fd59
knarfytrebil <knarfytrebil@gmail.com>
2022-11-28 20:58:54 +1030

    add styles

--- src/components/command_output.rs
+++ src/components/command_output.rs
@@ -12,7 +12,8 @@ use structs::app::AppState;
 
 const DATA: &'static str = r#"
 <Paragraph 
-    styles='{}' scroll='{"offset": [1, 20]}' wrap='{"trim": true}' alignment='{"position" : "Left"}'>
+    styles='{"fg": {"Color": "red"}}' 
+    scroll='{"offset": [1, 20]}' wrap='{"trim": true}' alignment='{"position" : "Left"}'>
     {{#each store.console_output_lines as |line| ~}}
         <Spans>
             <Span>{{line}}</Span>
