9d073591e9fb59be4114ad5d2b2e097ac87e66d7
knarfytrebil <knarfytrebil@gmail.com>
2023-11-20 22:15:49 +1030

    (wip) add walkdir library

--- src/utils/run.rs
+++ src/utils/run.rs
@@ -17,8 +17,8 @@ use ratatui::backend::TermionBackend;
 use ratatui::Terminal;
 
 pub fn keep_alive(receiver: Receiver<Event>) -> Result<(), io::Error> {
-    // Terminal initialization
 
+    // initialize terminal
     let stdout = io::stdout()
         .into_raw_mode()
         .unwrap()
@@ -29,6 +29,8 @@ pub fn keep_alive(receiver: Receiver<Event>) -> Result<(), io::Error> {
     let mut terminal = Terminal::new(backend)?;
     terminal.clear().unwrap();
     terminal.hide_cursor()?;
+
+    // initialize handbars template
     let mut reg = Handlebars::new();
     reg.register_helper("stringify", Box::new(hb_macros::stringify));
     reg.register_helper("powerline_symbol", Box::new(hb_macros::powerline_symbol));
@@ -40,7 +42,7 @@ pub fn keep_alive(receiver: Receiver<Event>) -> Result<(), io::Error> {
             Event::Render(app_state) => { 
                 _ = terminal.draw(|mut f| {
                     app::render(
-                        &mut f, 
+                        &mut f,
                         &app_state,
                         reg.clone()
                     )
