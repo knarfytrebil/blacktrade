dae6354ae2aea99dd87a9d28c01bc52a12ecff81
Yao Yuan <knarfytrebil@gmail.com>
2023-11-23 10:54:13 +1030

    remove excessive render function

--- src/components/app.rs
+++ src/components/app.rs
@@ -7,32 +7,6 @@ use components::parsing::xml::parse;
 use components::utils::props;
 use structs::app::AppState;
 
-
-pub fn render_component<'a>(
-    frame: &mut Frame, 
-    store: &AppState, 
-    reg: &mut Handlebars<'_>,
-    area: Option<Rect>, 
-    template: &'a str,
-) {
-
-    let dom_root = parse(
-        template,
-        &props(&store.json_store, area),
-        reg
-    );
-
-    match xml::create_element(dom_root) {
-        xml::El::Paragraph(p) => frame.render_widget(p, area.unwrap()),
-        xml::El::Tabs(t) => frame.render_widget(t, area.unwrap()),
-        xml::El::Layout(l) => {
-            l.split(frame.size());
-        },
-        _ => panic!("XML Parse Error !"),
-    };
-
-}
-
 pub fn render<'a>(
     frame: &mut Frame,
     store: &AppState,
@@ -46,18 +20,27 @@ pub fn render<'a>(
         reg
     );
     
-    let chunks = match xml::create_element( dom_root) {
-        xml::El::Layout(l) => l.split(frame.size()),
+    let chunks: Option<_>= match xml::create_element( dom_root) {
+        xml::El::Paragraph(p) => { 
+            frame.render_widget(p, area.unwrap());
+            None
+        },
+        xml::El::Tabs(t) => {
+            frame.render_widget(t, area.unwrap());
+            None
+        },
+        xml::El::Layout(l) => Some(l.split(frame.size())),
         _ => panic!("XML Parse Error !"),
     };
 
-    render_component(frame, store, reg, Some(chunks[0]), "tabs");
-    render_component(frame, store, reg, Some(chunks[2]), "status_bar");
-    render_component(frame, store, reg, Some(chunks[3]), "command_bar");
-    match store.tabs.selection {
-        0 => render_component(frame, store, reg, Some(chunks[1]), "command_output"),
-        1 => {}
-        _ => {}
+    if let Some(chunks) = chunks {
+        render(frame, store, reg, Some(chunks[0]), "tabs");
+        render(frame, store, reg, Some(chunks[2]), "status_bar");
+        render(frame, store, reg, Some(chunks[3]), "command_bar");
+        match store.tabs.selection {
+            0 => render(frame, store, reg, Some(chunks[1]), "command_output"),
+            1 => {}
+            _ => {}
+        }
     }
-
 }
